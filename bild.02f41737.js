95bc39c663ebd71f7c47e79c72a272061ce40047
Angelica Adolfsson <angelicaadolfsson1@gmail.com>
2025-03-06 14:10:40 +0100

    All files after npm build command

---
+++ bild.02f41737.js
@@ -0,0 +1,2 @@
+let e;e=new Date,document.getElementById("dateTime").innerHTML=e.toLocaleString(),console.log("Dagens datum:",current);
+//# sourceMappingURL=start.02f41737.js.map
---
+++ bild.25a30191.js
@@ -0,0 +1,2 @@
+let e=new Date;document.getElementById("dateTime").innerHTML=e.toLocaleString(),console.log("Dagens datum:",current);
+//# sourceMappingURL=start.25a30191.js.map
